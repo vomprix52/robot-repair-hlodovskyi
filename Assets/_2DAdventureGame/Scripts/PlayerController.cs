a71c0ec3befb4c3932010989d4e9fd9716ca14b7
vomprix52 <hlodovskyi.b.m_ipz24@rcit.ukr.education>
2026-09-20 23:42:00 +0300

    Юніт 1: персонаж і рух»

---
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction MoveAction;
+
+    void Start()
+    {
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+        MoveAction.Enable();
+    }
+
+    void Update()
+    {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position
+        + move * 3.0f * Time.deltaTime;
+        transform.position = position;
+    }
+}
